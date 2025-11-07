f511207a12a013e6a2a1bdb24610295ecfcdadf9
josedherna <157747148+josedherna@users.noreply.github.com>
2025-11-06 23:01:17 -0500

    feat: complete task 2, 3, and 4 of storing username and showing the values stored in the Datastore

--- app/src/main/java/edu/farmingdale/datastoresimplestoredemo/MainActivity.kt
+++ app/src/main/java/edu/farmingdale/datastoresimplestoredemo/MainActivity.kt
@@ -7,16 +7,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import edu.farmingdale.datastoresimplestoredemo.data.AppPreferences
 import edu.farmingdale.datastoresimplestoredemo.ui.theme.DataStoreSimpleStoreDemoTheme
@@ -68,23 +79,61 @@ fun DataStoreDemo(modifier: Modifier) {
     val store = AppStorage(LocalContext.current)
     val appPrefs = store.appPreferenceFlow.collectAsState(AppPreferences())
     val coroutineScope = rememberCoroutineScope()
+
+    //Task 1
+    var highScore by rememberSaveable { mutableStateOf("") }
+
+    //Task 1
+    var darkMode by rememberSaveable { mutableStateOf(false) }
+
+    //Task 2
+    var userName by rememberSaveable { mutableStateOf("") }
+
     Column (modifier = Modifier.padding(50.dp)) {
         Text("Values = ${appPrefs.value.userName}, " +
                 "${appPrefs.value.highScore}, ${appPrefs.value.darkMode}")
+        Spacer(Modifier.height(20.dp))
+        //Task 1
+        OutlinedTextField(
+            value = highScore,
+            onValueChange = {
+                highScore = it
+            },
+            label = {
+                Text("High Score")
+            }
+        )
+        Spacer(Modifier.height(20.dp))
+        //Task 1
+        Text("Dark Mode")
+        Switch(
+            checked = darkMode,
+            onCheckedChange = {
+                darkMode = it
+            })
+        Spacer(Modifier.height(20.dp))
+        //Task 2
+        OutlinedTextField(
+            value = userName,
+            onValueChange = {
+                userName = it
+            },
+            label = {
+                Text("Username")
+            }
+        )
+
+        //Task 3
         Button(onClick = {
             coroutineScope.launch {
-                store.saveUsername("somevaluehere")
+                store.saveUsername(userName)
+                store.saveHighscore(highScore)
+                store.saveDarkmode(darkMode)
             }
-
         }) {
             Text("Save Values")
         }
     }
 }
-
-// ToDo 1: Modify the App to store a high score and a dark mode preference
-// ToDo 2: Modify the APP to store the username through a text field
-// ToDo 3: Modify the App to save the username when the button is clicked
-// ToDo 4: Modify the App to display the values stored in the DataStore
 
 
